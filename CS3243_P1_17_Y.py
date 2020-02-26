dd6cc2b75a9459c6bc5052577476a9607f6b0f26
Chen Yi Jun <onearmy1997@gmail.com>
2020-02-26 14:16:47 +0800

    update base file

---
+++ CS3243_P1_17_Y.py
@@ -0,0 +1,75 @@
+import os
+import sys
+
+
+class Puzzle(object):
+    def __init__(self, init_state, goal_state):
+        # you may add more attributes if you think is useful
+        self.init_state = init_state
+        self.goal_state = goal_state
+        self.actions = list()
+
+    def solve(self):
+        #TODO
+        # implement your search algorithm here
+        
+        return ["LEFT", "RIGHT"] # sample output 
+
+    # you may add more functions if you think is useful
+
+if __name__ == "__main__":
+    # do NOT modify below
+
+    # argv[0] represents the name of the file that is being executed
+    # argv[1] represents name of input file
+    # argv[2] represents name of destination output file
+    if len(sys.argv) != 3:
+        raise ValueError("Wrong number of arguments!")
+
+    try:
+        f = open(sys.argv[1], 'r')
+    except IOError:
+        raise IOError("Input file not found!")
+
+    lines = f.readlines()
+    
+    # n = num rows in input file
+    n = len(lines)
+    # max_num = n to the power of 2 - 1
+    max_num = n ** 2 - 1
+
+    # Instantiate a 2D list of size n x n
+    init_state = [[0 for i in range(n)] for j in range(n)]
+    goal_state = [[0 for i in range(n)] for j in range(n)]
+    
+
+    i,j = 0, 0
+    for line in lines:
+        for number in line.split(" "):
+            if number == '':
+                continue
+            value = int(number , base = 10)
+            if  0 <= value <= max_num:
+                init_state[i][j] = value
+                j += 1
+                if j == n:
+                    i += 1
+                    j = 0
+
+    for i in range(1, max_num + 1):
+        goal_state[(i-1)//n][(i-1)%n] = i
+    goal_state[n - 1][n - 1] = 0
+
+    puzzle = Puzzle(init_state, goal_state)
+    ans = puzzle.solve()
+
+    with open(sys.argv[2], 'a') as f:
+        for answer in ans:
+            f.write(answer+'\n')
+
+
+
+
+
+
+
